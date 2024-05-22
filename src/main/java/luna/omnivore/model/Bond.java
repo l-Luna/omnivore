2907ad9a919172f94edb5c944e12c6ef53f6a1fc
l-Luna <izzeldeen03@gmail.com>
2024-05-22 19:13:38 +0100

    more `Molecule` helpers

--- src/main/java/luna/omnivore/model/Bond.java
+++ src/main/java/luna/omnivore/model/Bond.java
@@ -5,4 +5,8 @@ public record Bond(BondType type, HexIndex start, HexIndex end){
 	public Bond rotate(HexIndex around, int turns){
 		return new Bond(type, start.rotate(around, turns), end.rotate(around, turns));
 	}
+	
+	public Bond translate(HexIndex by){
+		return new Bond(type, start.add(by), end.add(by));
+	}
 }
--- src/main/java/luna/omnivore/model/Molecule.java
+++ src/main/java/luna/omnivore/model/Molecule.java
@@ -10,10 +10,38 @@ public record Molecule(Map<HexIndex, AtomType> atoms, Set<Bond> bonds){
 	}
 	
 	public Molecule rotate(HexIndex around, int turns){
+		if(turns == 0)
+			return this;
 		Map<HexIndex, AtomType> newAtoms = new HashMap<>(atoms.size());
 		for(Map.Entry<HexIndex, AtomType> entry : atoms.entrySet())
 			newAtoms.put(entry.getKey().rotate(around, turns), entry.getValue());
 		Set<Bond> newBonds = bonds.stream().map(x -> x.rotate(around, turns)).collect(Collectors.toSet());
 		return new Molecule(newAtoms, newBonds);
+	}
+	
+	public Molecule translate(HexIndex by){
+		Map<HexIndex, AtomType> newAtoms = new HashMap<>(atoms.size());
+		for(Map.Entry<HexIndex, AtomType> entry : atoms.entrySet())
+			newAtoms.put(entry.getKey().add(by), entry.getValue());
+		Set<Bond> newBonds = bonds.stream().map(x -> x.translate(by)).collect(Collectors.toSet());
+		return new Molecule(newAtoms, newBonds);
+	}
+	
+	public Molecule normalizeTranslation(){
+		// find the top-most atom and translate it to 0v, and the left-most and translate it to 0u
+		int minU = atoms.keySet().stream().mapToInt(HexIndex::u).min().getAsInt();
+		int minV = atoms.keySet().stream().mapToInt(HexIndex::v).min().getAsInt();
+		return translate(new HexIndex(-minU, -minV));
+	}
+	
+	public boolean equalsIgnoreTranslation(Molecule other){
+		return equals(other) || normalizeTranslation().equals(other.normalizeTranslation());
+	}
+	
+	public boolean equalsIgnoreRotation(Molecule other){
+		for(int i = 0; i < 6; i++)
+			if(rotate(HexIndex.ORIGIN, i).equalsIgnoreTranslation(other))
+				return true;
+		return false;
 	}
 }
