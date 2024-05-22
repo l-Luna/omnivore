409be697f5d19dca007bc8be9e46c0524677f67e
l-Luna <izzeldeen03@gmail.com>
2024-05-22 17:43:22 +0100

    `rotate` for `Bond` and `Molecule`

--- src/main/java/luna/omnivore/model/Bond.java
+++ src/main/java/luna/omnivore/model/Bond.java
@@ -1,3 +1,8 @@
 package luna.omnivore.model;
 
-public record Bond(BondType type, HexIndex start, HexIndex end){}
+public record Bond(BondType type, HexIndex start, HexIndex end){
+	
+	public Bond rotate(HexIndex around, int turns){
+		return new Bond(type, start.rotate(around, turns), end.rotate(around, turns));
+	}
+}
--- src/main/java/luna/omnivore/model/Molecule.java
+++ src/main/java/luna/omnivore/model/Molecule.java
@@ -1,10 +1,19 @@
 package luna.omnivore.model;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public record Molecule(Map<HexIndex, AtomType> atoms, Set<Bond> bonds){
 	
 	public Molecule(){
 		this(new HashMap<>(), new HashSet<>());
+	}
+	
+	public Molecule rotate(HexIndex around, int turns){
+		Map<HexIndex, AtomType> newAtoms = new HashMap<>(atoms.size());
+		for(Map.Entry<HexIndex, AtomType> entry : atoms.entrySet())
+			newAtoms.put(entry.getKey().rotate(around, turns), entry.getValue());
+		Set<Bond> newBonds = bonds.stream().map(x -> x.rotate(around, turns)).collect(Collectors.toSet());
+		return new Molecule(newAtoms, newBonds);
 	}
 }
